24508ddf7542e3f99f020b4bcde9703128ea19d6
donniebreve <donniebreve@gmail.com>
2018-07-13 14:06:52 -0700

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Initial installation script
 
-sudo pacman -S sway dmenu termite pulseaudio ttf-dejavu tamsyn-font git
+sudo pacman -S sway i3status termite pulseaudio ttf-dejavu tamsyn-font git
