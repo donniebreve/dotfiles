a1a982ab0ba0be3d1bb5fad976f94e66385c38bc
donniebreve <donniebreve@protonmail.com>
2024-01-30 22:50:13 -0800

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,14 +17,19 @@
 (savehist-mode 1)                     ; Save minibuffer history
 
 (column-number-mode)                  ; Show column number in modeline
-(global-display-line-numbers-mode t)  ; Show line numbers everywhere
 (global-visual-line-mode 1)           ; Always use visual lines (never logical lines)
 (display-time-mode t)                 ; Displays the time in the modeline
+
+(global-display-line-numbers-mode t)  ; Show line numbers everywhere
+(setq display-line-numbers-width 3)   ; Use at least 3 columns for line numbers
 
 (setq inhibit-startup-message t)      ; Don't show start up screen
 (setq visible-bell nil)               ; Don't flash
 (setq ring-bell-function 'ignore)     ; Don't ring
 (setopt use-short-answers t)          ; Use y/n instead of yes/no
+
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 (setq vc-handled-backends '(Git))
 
 ;; Sets the margin while scrolling (vim=scrolloff)
@@ -150,6 +155,16 @@ are open."
   (general-auto-unbind-keys)
   (general-evil-setup))
 
+;; avy (movement)
+;; https://github.com/abo-abo/avy
+(use-package avy
+  :config
+  (avy-setup-default)
+  :general
+  (:states '(normal)
+       "gss" 'evil-avy-goto-char-2
+       "gs/" 'evil-avy-goto-char-timer))
+
 ;; company (completion)
 ;; https://github.com/company-mode/company-mode
 (use-package company
@@ -157,16 +172,23 @@ are open."
   (prog-mode . company-mode)
   (text-mode . company-mode)
   :general
-  (:keymaps
-   '(company-active-map)
-   "<escape>" '(lambda () (interactive) (company-abort) (evil-normal-state)))) ;; esc should abort company and insert mode
+  (:keymaps '(company-active-map)
+    "<escape>" '(lambda () (interactive) (company-abort) (evil-normal-state)))) ;; esc should abort company and insert mode
 
 ;; consult (search, completion)
 ;; suite of useful commands using `completing-read`
 ;; https://github.com/minad/consult
 (use-package consult
   :config
-  (setq completion-in-region-function #'consult-completion-in-region)) ;; the pcomplete-list window is awful! mouse!? X.X
+  (setq completion-in-region-function #'consult-completion-in-region) ;; the pcomplete-list window is awful! mouse!? X.X
+  (setq consult-grep-args '("rg"
+                             "--iglob !.#* --iglob !*.o --iglob !*~ --iglob !*.bin --iglob !*.bak --iglob !*.obj --iglob !*.map --iglob !*.ico --iglob !*.pif --iglob !*.lnk --iglob !*.a --iglob !*.ln --iglob !*.blg --iglob !*.bbl --iglob !*.dll --iglob !*.drv --iglob !*.vxd --iglob !*.386 --iglob !*.elc --iglob !*.lof --iglob !*.glo --iglob !*.idx --iglob !*.lot --iglob !*.fmt --iglob !*.tfm --iglob !*.class --iglob !*.fas --iglob !*.lib --iglob !*.mem --iglob !*.x86f --iglob !*.sparcf --iglob !*.dfsl --iglob !*.pfsl --iglob !*.d64fsl --iglob !*.p64fsl --iglob !*.lx64fsl --iglob !*.lx32fsl --iglob !*.dx64fsl --iglob !*.dx32fsl --iglob !*.fx64fsl --iglob !*.fx32fsl --iglob !*.sx64fsl --iglob !*.sx32fsl --iglob !*.wx64fsl --iglob !*.wx32fsl --iglob !*.fasl --iglob !*.ufsl --iglob !*.fsl --iglob !*.dxl --iglob !*.lo --iglob !*.la --iglob !*.gmo --iglob !*.mo --iglob !*.toc --iglob !*.aux --iglob !*.cp --iglob !*.fn --iglob !*.ky --iglob !*.pg --iglob !*.tp --iglob !*.vr --iglob !*.cps --iglob !*.fns --iglob !*.kys --iglob !*.pgs --iglob !*.tps --iglob !*.vrs --iglob !*.pyc --iglob !*.pyo --iglob !SCCS --iglob !RCS --iglob !CVS --iglob !MCVS --iglob !.src --iglob !.svn --iglob !.git --iglob !.hg --iglob !.bzr --iglob !_MTN --iglob !_darcs --iglob !{arch}"
+                             "--ignore-case --null --line-buffered --color=never --max-columns=1000 --path-separator / --smart-case --no-heading --with-filename --line-number --search-zip --hidden -g !.git -g !.svn -g !.hg"))
+  :general
+  (:states '(normal)
+       :keymaps '(override)
+       :prefix "SPC"
+       "p/" '(consult-ripgrep :which-key "Find in project")))
 
 ;; dashboard (ui)
 ;; https://github.com/emacs-dashboard/emacs-dashboard
@@ -174,11 +196,11 @@ are open."
   :init
   (setq dashboard-startup-banner "~/.emacs.d/banners/sub-zero.txt")
   (setq dashboard-center-content t)
+  (setq dashboard-filter-agenda-entry 'dashboard-no-filter-agenda)
   (setq dashboard-items '((recents  . 5)
                            (projects . 5)
                            (agenda . 5)
-                           (bookmarks . 5)
-                           (registers . 5)))
+                           (bookmarks . 5)))
   :config
   (dashboard-setup-startup-hook))
 
@@ -193,18 +215,22 @@ are open."
   :straight
   (:type built-in)
   :init
+  (setq dired-omit-files "\\`[.]\\|\\`[.]?#\\|\\`[.][.]?\\'")
   (setq dired-kill-when-opening-new-dired-buffer t)
+  :hook
+  (dired-mode . dired-omit-mode)
   :general
   (:states '(normal)
-	   :keymaps '(override)
-	   :prefix "SPC"
-	   "fd" '(dired-jump :which-key "Find directory"))
+	  :keymaps '(override)
+	  :prefix "SPC"
+	  "fd" '(dired-jump :which-key "Find directory"))
   (:states '(normal)
-     :keymaps '(dired-mode-map)
-     "h" '(dired-up-directory :which-key "Go up a directory")
-     "j" '(dired-next-line :which-key "Next line")
-     "k" '(dired-previous-line :which-key "Previous line")
-     "l" '(dired-find-file :which-key "Open file or directory")))
+    :keymaps '(dired-mode-map)
+    "h"  '(dired-up-directory :which-key "Go up a directory")
+    "j"  '(dired-next-line :which-key "Next line")
+    "k"  '(dired-previous-line :which-key "Previous line")
+    "l"  '(dired-find-file :which-key "Open file or directory")
+    "zh" '(dired-omit-mode :which-key "Toggle file hiding")))
 
 ;; dired-fl (appearance)
 ;; additional highlighting for dired
@@ -261,8 +287,29 @@ are open."
         :executeCommandProvider		;Execute custom commands
         :inlayHintProvider		;Inlay hints
 	))
+  :config
+  (add-to-list 'eglot-server-programs
+    '(vue-mode . ("vue-language-server" "--stdio" :initializationOptions
+                   (:typescript (:tsdk "/usr/lib/node_modules/typescript/lib"
+                                 :languageFeatures (:completion
+                                                     (:defaultTagNameCase "both"
+                                                       :defaultAttrNameCase "kebabCase"
+                                                       :getDocumentNameCasesRequest nil
+                                                       :getDocumentSelectionRequest nil)
+                                                     :diagnostics
+                                                     (:getDocumentVersionRequest nil))
+                                 :documentFeatures (:documentFormatting
+                                                     (:defaultPrintWidth 100
+                                                       :getDocumentPrintWidthRequest nil)
+                                                     :documentSymbol t
+                                                     :documentColor t))))))
   :hook
-  (csharp-ts-mode . eglot-ensure))
+  (c-mode . eglot-ensure)
+  (c-ts-mode . eglot-ensure)
+  (csharp-mode . eglot-ensure)
+  (csharp-ts-mode . eglot-ensure)
+  (typescript-ts-mode . eglot-ensure)
+  (vue-mode . eglot-ensure))
 
 ;; eldoc
 (use-package eldoc
@@ -296,7 +343,6 @@ are open."
 ;; esup (profiling)
 ;; https://github.com/jschaf/esup
 (use-package esup
-  :defer t
   :init
   (setq esup-depth 0)) ;; see https://github.com/jschaf/esup/issues/85
 
@@ -344,11 +390,45 @@ are open."
   :after evil
   :config
   (evil-collection-init))
+(use-package evil-commentary
+  :demand t
+  :after evil
+  :config
+  (evil-commentary-mode))
+(use-package evil-surround
+  :demand t
+  :after evil
+  :config
+  (global-evil-surround-mode 1))
+(use-package evil-textobj-anyblock
+  :demand t
+  :after evil
+  :config
+  ;; Remove quotes from blocks
+  (setq evil-textobj-anyblock-blocks '(("(" . ")") ("{" . "}") ("\\[" . "\\]") ("<" . ">"))) 
+  ;; Set up quotes separately
+  (setq evil-textobj-anyblock-quotes '(("'" . "'") ("\"" . "\"") ("`" . "`") ("“" . "”")))
+  (evil-define-text-object evil-textobj-anyblock-inner-quote
+    (count &optional beg end type)
+    "Select the closest outer quote."
+    (let ((evil-textobj-anyblock-blocks evil-textobj-anyblock-quotes))
+      (evil-textobj-anyblock--make-textobj beg end type count nil)))
+  (evil-define-text-object evil-textobj-anyblock-a-quote
+    (count &optional beg end type)
+    "Select the closest outer quote."
+    (let ((evil-textobj-anyblock-blocks evil-textobj-anyblock-quotes))
+      (evil-textobj-anyblock--make-textobj beg end type count t)))
+  :general
+  (:keymaps '(evil-inner-text-objects-map)
+    "b" 'evil-textobj-anyblock-inner-block
+    "q" 'evil-textobj-anyblock-inner-quote)
+  (:keymaps '(evil-outer-text-objects-map)
+    "b" 'evil-textobj-anyblock-a-block
+    "q" 'evil-textobj-anyblock-a-quote))
 
 ;; helpful (help)
 ;; https://github.com/Wilfred/helpful
 (use-package helpful
-  :defer t
   :general
   (:states '(normal)
      :keymaps '(override)
@@ -360,18 +440,19 @@ are open."
 
 ;; hl-todo
 (use-package hl-todo
-  :hook (prog-mode . hl-todo-mode)
-  :hook (yaml-mode . hl-todo-mode)
   :config
   (setq hl-todo-highlight-punctuation ":"
-        hl-todo-keyword-faces
-        '(("TODO" warning bold)
-          ("DEPRECATED" font-lock-doc-face bold)
-          ("REVIEW" font-lock-keyword-face bold)
-          ("NOTE" success bold)
-          ("HACK" font-lock-constant-face bold)
-	  ("FIXME" error bold)
-          ("BUG" error bold))))
+    hl-todo-keyword-faces
+    '(("TODO" warning bold)
+       ("DEPRECATED" font-lock-doc-face bold)
+       ("REVIEW" font-lock-keyword-face bold)
+       ("NOTE" success bold)
+       ("HACK" font-lock-constant-face bold)
+	     ("FIXME" error bold)
+       ("BUG" error bold)))
+  :hook
+  (prog-mode . hl-todo-mode)
+  (yaml-mode . hl-todo-mode))
 
 ;; magit (version control)
 ;; https://github.com/magit/magit
@@ -409,35 +490,34 @@ are open."
 ;; https://github.com/alphapapa/magit-todos
 (use-package magit-todos
   :after magit
-  :defer t
   :init
   (if (eq system-type 'windows-nt)
-      (setq magit-todos-nice nil)) ;; don't use `nice` on windows, it doesn't exist T_T
+    (setq magit-todos-nice nil)) ;; don't use `nice` on windows, it doesn't exist T_T
   :config
   ;; https://github.com/alphapapa/magit-todos/issues/156
   (if (eq system-type 'windows-nt)
-      (setq magit-todos-scanners nil)
+    (setq magit-todos-scanners nil)
     (magit-todos-defscanner "rg"
-			    :test (executable-find "rg")
-			    :directory-form (f-relative directory default-directory) ;; revert
-			    :allow-exit-codes (0 1)
-			    :command (list "rg" "--no-heading" "--line-number"
-					   (when depth
-					     (list "--maxdepth" (1+ depth)))
-					   (when magit-todos-ignore-case
-					     "--ignore-case")
-					   (when magit-todos-exclude-globs
-					     (--map (list "--glob" (concat "!" it))
-						    magit-todos-exclude-globs))
-					   (unless magit-todos-submodule-list
-					     (--map (list "--glob" (concat "!" it))
-						    (magit-list-module-paths)))
-					   extra-args search-regexp-pcre directory)))
+			:test (executable-find "rg")
+			:directory-form (f-relative directory default-directory) ;; revert
+			:allow-exit-codes (0 1)
+			:command (list "rg" "--no-heading" "--line-number"
+					       (when depth
+					         (list "--maxdepth" (1+ depth)))
+					       (when magit-todos-ignore-case
+					         "--ignore-case")
+					       (when magit-todos-exclude-globs
+					         (--map (list "--glob" (concat "!" it))
+						         magit-todos-exclude-globs))
+					       (unless magit-todos-submodule-list
+					         (--map (list "--glob" (concat "!" it))
+						         (magit-list-module-paths)))
+					       extra-args search-regexp-pcre directory)))
   :general
   (:states '(normal)
-	   :keymaps '(override)
-	   :prefix "SPC"
-	   "pt" '(magit-todos-list :which-key "List project todos")))
+	  :keymaps '(override)
+	  :prefix "SPC"
+	  "pt" '(magit-todos-list :which-key "List project todos")))
 
 ;; marginalia.el (completion, annotations) 
 ;; provides annotations for completion candidates
@@ -445,14 +525,6 @@ are open."
 (use-package marginalia
   :init
   (marginalia-mode))
-
-;; mood-line
-;; https://gitlab.com/jessieh/mood-line
-(use-package mood-line
-  :config
-  (mood-line-mode)
-  :custom
-  (mood-line-glyph-alist mood-line-glyphs-fira-code))
 
 ;; markdown (coding, writing)
 ;; https://jblevins.org/projects/markdown-mode
@@ -462,6 +534,16 @@ are open."
   ("README\\.md\\'" . gfm-mode)
   :init
   (setq markdown-command "multimarkdown"))
+
+;; mood-line
+;; https://gitlab.com/jessieh/mood-line
+(use-package mood-line
+  :config
+  (set-face-attribute 'mood-line-unimportant nil
+    :foreground "#839496")
+  (mood-line-mode)
+  :custom
+  (mood-line-glyph-alist mood-line-glyphs-fira-code))
 
 ;; orderless (completion, filtering)
 ;; better filtering methods
@@ -477,14 +559,18 @@ are open."
   :straight
   (:type built-in)
   :config
-  (setq org-directory "~/Documents/Notes"))
+  (setq org-directory "~/Documents/Notes")
+  (setq org-agenda-files '("~/Documents/Notes"))
+  :general
+  (:keymaps '(org-agenda-mode-map)
+    "j" 'evil-next-line
+    "k" 'evil-previous-line))
 
-;; perp-mode (completion, buffers)
-;; group related buffers (e.g. for a project)
+;; persp-mode (buffers)
 ;; https://github.com/Bad-ptr/persp-mode.el
 (use-package persp-mode
   :init
-  (persp-mode))
+  (setq persp-auto-save-opt 0))
 
 ;; prettier.el (formatting, coding)
 ;; prettier code formatting
@@ -551,6 +637,14 @@ are open."
 	   :keymaps '(override)
 	   :prefix "SPC"
 	   "'" '(vertico-repeat :which-key "Resume last search")))
+
+;; web-mode (coding)
+;; https://github.com/fxbois/web-mode
+(use-package web-mode
+  :init
+  (define-derived-mode vue-mode web-mode "Vue")
+  :mode
+  ("\\.vue\\'" . vue-mode))
 
 ;; which-key.el (key bindings)
 ;; https://github.com/justbur/emacs-which-key
