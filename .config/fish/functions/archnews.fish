01ac2422b4ad1c1b17588e961cc2a581e9c63810
donniebreve <donniebreve@protonmail.com>
2024-11-10 16:44:57 -0800

    Update archnews.fish

--- .config/fish/functions/archnews.fish
+++ .config/fish/functions/archnews.fish
@@ -25,6 +25,21 @@ function archnews_hyperlink
     echo -e "\033]8;;$argv[1]\033\\$argv[2]\033]8;;\033\\"
 end
 
+function archnews_should_refresh
+    set file $argv[1]
+    if test -e $file
+        set current_time (date +%s)
+        set file_mod_time (stat -c %Y $file)
+        if test (math "$current_time - $file_mod_time") -gt 3600
+            true
+        else
+            false
+        end
+    else
+        true
+    end
+end
+
 function archnews_format_item
     set title (echo $argv[1] | xmllint --xpath 'string(//title)' -)
     set date (echo $argv[1] | xmllint --xpath 'string(//pubDate)' -)
@@ -46,8 +61,8 @@ function archnews -d 'arch news commands'
     argparse -n archnews h/help 'l/long=' 's/short=' -- $argv
     or return
 
-    set long 0
-    set short 0
+    set long -1
+    set short -1
 
     if set -q _flag_help
         echo "Print Arch Linux's news RSS feed"
@@ -58,29 +73,33 @@ function archnews -d 'arch news commands'
         echo "Example: archnews --long 0 --short 2: Shortly print two items"
     end
 
+    set tmp $TMPDIR
+    if test -z "$TMPDIR"
+        set tmp /tmp
+    end
+    set archnews_file "$tmp/archnews.rss"
+    if archnews_should_refresh $archnews_file
+        set response (curl -s "https://archlinux.org/feeds/news/")
+        echo $response > $archnews_file
+    else
+        set response (cat $archnews_file)
+    end
+
+    set items (echo $response | xmllint --xpath '//item' -)
+
     if set -q _flag_long
         set long $_flag_long
+    else
+        set long 3 
+    end
+
+    for i in (seq 1 $long)
+        set item $items[$i]
+        archnews_format_item $item
     end
 
     if set -q _flag_short
         set short $_flag_short
-    end
-
-    if test $long -le 0 -a $short -le 0
-        return
-    end
-
-    set response (curl -s "https://archlinux.org/feeds/news/")
-    set items (echo $response | xmllint --xpath '//item' -)
-
-    if test $long -gt 0
-        for i in (seq 1 $long)
-            set item $items[$i]
-            archnews_format_item $item
-        end
-    end
-
-    if test $short -gt 0
         set end (math "$long + $short")
         set start (math "$long + 1")
         for i in (seq $start $end)
