3fc50ed1cc053c1b276bb6452e4d0f8103cdeb44
donniebreve <donniebreve@protonmail.com>
2025-02-09 14:29:47 -0800

    added nvim config to sync script

--- sync.sh
+++ sync.sh
@@ -2,3 +2,6 @@
 
 # Sway config
 rsync --verbose --delete --recursive ~/.config/sway/ ./.config/sway/
+
+# Neovim config
+rsync --verbose --delete --recursive --exclude lazy-lock.json ~/.config/nvim/ ./.config/nvim/
